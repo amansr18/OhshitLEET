7b026aeb1f76c0b99dcac01caa0db3825bf0193c
AmanSR <87056328+amansr18@users.noreply.github.com>
2022-11-08 23:50:25 +0530

    Time: 6 ms (48.12%), Space: 6.3 MB (65.67%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string makeGood(string s) {
+       
+        auto it=s.begin();
+        while(it<s.end() && s.size()>0)
+        {
+           if((abs(*it - *(it+1))==32))
+           {
+               s.erase(it,it+2);
+               it=s.begin();
+           }
+           else
+           {
+             it++;
+           }
+           
+        }
+        return s;
+    }
+};
