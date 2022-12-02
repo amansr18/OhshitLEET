195173884c92f78f54840e545ff805654aa5f8e8
AmanSR <87056328+amansr18@users.noreply.github.com>
2022-12-02 23:13:18 +0530

    Time: 136 ms (82.34%), Space: 20.8 MB (32.93%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,24 +1,41 @@
 class Solution {
-public:
-    bool closeStrings(string word1, string word2) {
-        if(word1.length()!=word2.length()) return false;
-        
-        vector<int> freq1(26,0), freq2(26,0);
-        
-        for(int i=0;i<word1.length();i++){
-            freq1[word1[i]-'a']++;
-            freq2[word2[i]-'a']++;
-        }
-        
-        for(int i=0;i<26;i++){
-            if(freq1[i]==0 ^ freq2[i]==0) return false;
-        }
-        sort(freq1.begin(), freq1.end());
-        sort(freq2.begin(), freq2.end());
-        
-        for(int i=0;i<26;i++){
-            if(freq1[i] != freq2[i]) return false;
-        }
-        return true;
+ public:
+  bool closeStrings(string word1, string word2) {
+    if (word1.length() != word2.length())
+      return false;
+
+    unordered_map<char, int> count1;
+    unordered_map<char, int> count2;
+    string s1;           // Unique chars in word1
+    string s2;           // Unique chars in word2
+    vector<int> freqs1;  // Freqs of unique chars in word1
+    vector<int> freqs2;  // Freqs of unique chars in word2
+
+    for (const char c : word1)
+      ++count1[c];
+
+    for (const char c : word2)
+      ++count2[c];
+
+    for (const auto& [c, freq] : count1) {
+      s1 += c;
+      freqs1.push_back(freq);
     }
+
+    for (const auto& [c, freq] : count2) {
+      s2 += c;
+      freqs2.push_back(freq);
+    }
+
+    sort(begin(s1), end(s1));
+    sort(begin(s2), end(s2));
+
+    if (s1 != s2)
+      return false;
+
+    sort(begin(freqs1), end(freqs1));
+    sort(begin(freqs2), end(freqs2));
+
+    return freqs1 == freqs2;
+  }
 };
