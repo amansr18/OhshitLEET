9008f7848e4c60a8ed4d5f6de91c41e8d95e4ae9
AmanSR <87056328+amansr18@users.noreply.github.com>
2023-01-30 23:04:58 +0530

    Time: 3 ms (38.83%), Space: 6 MB (73.21%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int dp[3]={0,1,1};
+    
+    //After given numbers calculate next Tribonacci number
+    //Save it at i%3th place
+    for(auto i=3;i<=n;i++)
+        dp[i%3]=dp[0]+dp[1]+dp[2];
+    
+    return dp[n%3];
+    }
+};
