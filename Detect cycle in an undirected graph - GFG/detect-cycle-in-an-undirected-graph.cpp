5f243469098788b80d1e6e68a872f6735e881243
AmanSR <87056328+amansr18@users.noreply.github.com>
2023-04-22 16:42:21 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -5,47 +5,42 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   private: 
-    bool detect(int src, vector<int> adj[], int vis[]) {
-      vis[src] = 1; 
-      // store <source node, parent node>
-      queue<pair<int,int>> q; 
-      q.push({src, -1}); 
-      // traverse until queue is not empty
-      while(!q.empty()) {
-          int node = q.front().first; 
-          int parent = q.front().second; 
-          q.pop(); 
-          
-          // go to all adjacent nodes
-          for(auto adjacentNode: adj[node]) {
-              // if adjacent node is unvisited
-              if(!vis[adjacentNode]) {
-                  vis[adjacentNode] = 1; 
-                  q.push({adjacentNode, node}); 
-              }
-              // if adjacent node is visited and is not it's own parent node
-              else if(parent != adjacentNode) {
-                  // yes it is a cycle
-                  return true; 
-              }
-          }
-      }
-      // there's no cycle
-      return false; 
-  }
+    bool detectcycle(int src, vector<int> adj[], vector<int> &vis){
+        queue<pair<int,int>> q;
+        
+        vis[src]=1;
+        q.push({src,-1});
+        
+        while(!q.empty()){
+            int node = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+            
+            for(auto it: adj[node]){
+                if(!vis[it]){
+                    q.push({it, node});
+                    vis[it]=1;
+                }
+                else if(parent != it) return true;
+            }
+        }
+        return false;
+        
+    }
     
     
   public:
     // Function to detect cycle in an undirected graph.
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
-        int vis[V] = {0};
-        for(int i = 0;i<V;i++) {
-            if(!vis[i]) {
-                if(detect(i, adj, vis)) return true; 
+        vector<int> vis(V,0);
+        
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                if(detectcycle(i, adj, vis)) return true;
             }
         }
-        return false; 
+        return false;
     }
 };
 
