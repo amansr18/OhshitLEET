2ad812ae2450406def6f06a8d910415e71629029
AmanSR <87056328+amansr18@users.noreply.github.com>
2023-05-10 20:38:05 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (70.13%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> matrix(n, vector<int> (n,0));
+        
+        int val=1;
+        int r1=0, c1=0, r2=n-1, c2=n-1;
+        
+        for (int min = 0; min < n / 2; ++min) {
+            for(int i=c1; i<=c2; i++){
+                matrix[r1][i]=val++;
+            }
+            for(int i=r1+1; i<=r2; i++){
+                matrix[i][c2]=val++;
+            }
+            for(int i=c2-1; i>=c1; i--){
+                matrix[r2][i]=val++;
+            }
+            for(int i=r2-1; i>r1; i--){
+                matrix[i][c1]=val++;
+            }
+            r1++;c1++;r2--;c2--;
+        }
+        
+        if (n & 1)
+            matrix[n / 2][n / 2] = val;
+        return matrix;
+    }
+};
