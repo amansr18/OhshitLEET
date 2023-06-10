8d9214c37623c41a2c4da24f05a4a11ddf24a03f
AmanSR <87056328+amansr18@users.noreply.github.com>
2023-06-11 02:56:15 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -5,26 +5,15 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 private:
-    bool check(int start, int V, int color[], vector<int>adj[]){
-        queue<int> q;
-        color[start]=0;
-        q.push(start);
-        
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            
-            for(auto it: adj[node]){
-                if(color[it]==-1){
-                    color[it]= !color[node];
-                    q.push(it);
-                }
-                else if(color[it]==color[node]){
-                    return false;
-                }
-            }
-        }
-        return true;
+    bool dfs(int clr, int start, int color[], vector<int>adj[]){
+       color[start]=clr;
+       for(auto it: adj[start]){
+               if(color[it]==-1){
+                   if(dfs(!clr, it, color, adj)==false) return false;
+               }
+               else if(color[it]==clr) return false;
+           }
+       return true;
     }
 
 public:
@@ -35,7 +24,7 @@ public:
 	    
 	    for(int i=0;i<V;i++){
 	        if(color[i]==-1){
-	            if(check(i, V, color, adj)==false){
+	            if(dfs(0, i, color, adj)==false){
 	                return false;
 	            }
 	        }
